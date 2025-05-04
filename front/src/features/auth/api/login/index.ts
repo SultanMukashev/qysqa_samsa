b965e6812ea7e390963eae84dcd99f115c74f963
Ansar <nansarik@mail.ru>
2025-05-04 06:15:10 +0600

    Refactor login function for improved readability and consistency. Updated error handling to ensure proper message formatting and added missing semicolons.

--- front/src/features/auth/api/login/index.ts
+++ front/src/features/auth/api/login/index.ts
@@ -3,24 +3,26 @@ import { ApiResponseMessage } from "@/shared/types";
 import { LoginRequest, LoginResponse } from "./type";
 import { AxiosError } from "axios";
 
-export async function login (request: LoginRequest): Promise<ApiResponseMessage<LoginResponse>> {
-  try{
-    const response = await api.post<LoginResponse>('students/auth', request)
+export async function login(
+  request: LoginRequest
+): Promise<ApiResponseMessage<LoginResponse>> {
+  try {
+    const response = await api.post<LoginResponse>("students/auth", request);
 
     return {
       ok: true,
       data: response.data,
-      message: 'Successfully loged in!'
-    }
+      message: "Successfully loged in!",
+    };
   } catch (error) {
     if (error instanceof AxiosError && error.response) {
-      const message = error.response.data?.message || "Something went wrong!"
+      const message = error.response.data?.message || "Something went wrong!";
       return {
         ok: false,
         data: null,
-        message: message
-      }
+        message: message,
+      };
     }
-    throw new Error("Network error. Please try again.")
+    throw new Error("Network error. Please try again.");
   }
 }
